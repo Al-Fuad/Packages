87a39403633fd4cd69ef8a816695bd4ef9cc9eb5
Al-Fuad <smalfuadnur@gmail.com>
2026-03-06 01:12:47 +0600

    feat: Add BSD 3-Clause License and update navbar documentation

--- flutter/navbar/lib/navbar.dart
+++ flutter/navbar/lib/navbar.dart
@@ -1,14 +1,27 @@
+/// A customizable and responsive navigation bar for Flutter applications.
+library navbar;
+
+/// Exporting the main Navbar class for external use.
 class Navbar {
+
+  /// [items] - A list of strings representing the navigation items.
+  /// [currentIndex] - The index of the currently selected item.
+  /// [onTap] - A callback function that is triggered when an item is tapped.
   final List<String> items;
   final int currentIndex;
   final Function(int) onTap;
 
+  /// Creates a new instance of the Navbar class.
   Navbar({
     required this.items,
     required this.currentIndex,
     required this.onTap,
   });
 
+  /// Handles tap events on the navigation items and triggers the provided callback.
+  ///
+  /// [index] - The index of the tapped item.
+  /// This method calls the `onTap` callback with the index of the tapped item, allowing the parent widget to update the state accordingly.
   void handleTap(int index) {
     onTap(index);
   }
